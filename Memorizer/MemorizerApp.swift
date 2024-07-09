b0600ac216dc682f5a51a5356215bb1b31fe6ffe
josimarcf2020 <josimarcf2020@gmail.com>
2024-07-09 19:06:21 -0300

    Initial Commit

---
+++ Memorizer/MemorizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizerApp.swift
+//  Memorizer
+//
+//  Created by Josimar da Cunha Ferreira on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
