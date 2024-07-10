f161c31eb5e2692304064a6662ddb6075b56c846
josimarcf2020 <josimarcf2020@gmail.com>
2024-07-10 18:01:11 -0300

    First commit

--- Memorizer/ContentView.swift
+++ Memorizer/ContentView.swift
@@ -21,19 +21,20 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @ State var isFaceUp: Bool = false
     var body: some View {
         ZStack {
+            let card: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                card.fill(.white)
+                card.strokeBorder(lineWidth: 2)
                 Text("👻").font(.largeTitle)
             } else {
                 RoundedRectangle(cornerRadius: 12)
             }
             
+        }.onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
